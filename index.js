9e54ab336802dec125d9b8627fd7d232d4705f3a
Carmen Johnson <carmenjohnson512@gmail.com>
2020-10-08 01:33:56 -0500

    add/view functionality added for dept, role and employee

--- index.js
+++ index.js
@@ -1,194 +1,318 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+var express = require("express");
+var exphbs = require("express-handlebars");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
-var connection = mysql.createConnection({
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+const connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "password",
   database: "employee_trackerDB"
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  runSearch();
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+  
+    console.log("connected as id " + connection.threadId);
+    start();
+  });
+
+function start() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update employee role",
+          "Exit"
+        ]
+      })
+    .then(function(answer) {
+        if (answer.action === 'View all departments') {
+            viewDepartments();
+        } else if (answer.action === 'View all roles') {
+            viewRoles();
+        } else if (answer.action === 'View all employees') {
+            viewEmployees();
+        } else if (answer.action === 'Add a department') {
+            addDepartment();
+        } else if (answer.action === 'Add a role') {
+            addRole();
+        } else if (answer.action === 'Add an employee') {
+            addEmployee();
+        } else if (answer.action === 'Update employee role') {
+            updateRole();
+        }
+        else if (answer.action === 'Exit') {
+            connection.end();
+        }
+    });
+}
+
+
+function viewDepartments() {
+    var query = "SELECT * FROM department";
+      connection.query(query, function(err, res) {
+          if(err) throw (err);
+        res.forEach(department => {
+            console.table(`ID: ${department.id} | Name: ${department.name}`)
+        });
+        connection.end();
+    });
+};
+
+function viewRoles() {
+    var query = "SELECT * FROM role";
+        connection.query(query, function(err, res) {
+            if(err) throw (err);
+            // console.log(`ROLES:`)
+        res.forEach(role => {
+            console.table(`ID: ${role.id} | Title: ${role.title} | Salary: ${role.salary} | Department ID: ${role.department_id}`);
+        });
+        });
+        connection.end();
+    };
+
+function viewEmployees() {
+    var query = "SELECT * FROM employee";
+        connection.query(query, function(err, res) {
+            if(err) throw (err);
+            console.log(`EMPLOYEES:`)
+        res.forEach(employee => {
+            console.table(`ID: ${employee.id} | Name: ${employee.first_name} ${employee.last_name} | Role ID: ${employee.role_id} | Manager ID: ${employee.manager_id}`);
+        })
+        });
+        connection.end();
+    };
+
+function addDepartment() {
+    inquirer
+        .prompt({
+            name: "department",
+            type: "input",
+            message: "What is the name of the new department?",
+          })
+        .then(function(answer) {
+        var query = "INSERT INTO department (name) VALUES ( ? )";
+        connection.query(query, answer.department, function(err, res) {
+            console.log(`You have added this department: ${(answer.department).toUpperCase()}.`)
+        })
+        viewDepartments();
+        })
+        connection.end();
+}
+
+function addRole() {
+    connection.query('SELECT * FROM department', function(err, res) {
+        if (err) throw (err);
+        inquirer
+            .prompt([{
+                name: "title",
+                type: "input",
+                message: "What is the title of the new role?",
+            }, 
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the salary of the new role?",
+            },
+            {
+                name: "departmentName",
+                type: "list",
+                message: "Which department does this role fall under?",
+                choices: function() {
+                    let choicesArray = [];
+                    res.forEach(res => {
+                        choicesArray.push(res.name);
+                    })
+                    return choicesArray;
+                }
+            }
+        ]) 
+    
+// in order to get the id here, i need a way to grab it from the departments table 
+        .then(function(answer) {
+            const department = answer.departmentName;
+            connection.query('SELECT * FROM department', function(err, res) {
+            
+                if (err) throw (err);
+                let filteredDept = res.filter(function(res) {
+                    return res.name == department;
+                })
+
+                let id = filteredDept[0].id;
+                let query = "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
+                let values = [answer.title, parseInt(answer.salary), id]
+                console.log(values);
+                
+                // viewRoles();
+
+
+            connection.query(query, values, function(err, res) {
+                let rolesArr = [];
+                if (err) throw (err);
+                // res.forEach(role => {
+                //     rolesArr.push(role.values);
+                // })
+                console.log(`You have added this role: ${(values[0])}.`)
+            })
+            connection.end();
+            })
+        })
+    })
+};
+
+function addEmployee() {
+    connection.query('SELECT * FROM role', function(err, result) {
+        if (err) throw (err);
+    inquirer
+        .prompt([{
+            name: "firstName",
+            type: "input",
+            message: "What is the employee's first name?",
+          }, 
+          {
+            name: "lastName",
+            type: "input",
+            message: "What is the employee's last name?",
+          },
+          {
+            name: "roleName",
+            type: "list",
+            message: "What role does the employee have?",
+            choices: function() {
+             let rolesArray = [];
+                result.forEach(result => {
+                    rolesArray.push(
+                        result.title
+                    );
+                })
+                return rolesArray;
+              }
+          },
+        //   {
+        //     name: "isManager",
+        //     type: "list",
+        //     message: "Is this employee a manager?",
+        //     choices: ["YES", "NO"]
+        //   }
+        ]) 
+// in order to get the id here, i need a way to grab it from the departments table 
+        .then(function(answer) {
+            console.log(answer);
+            const role = answer.roleName;
+            connection.query('SELECT * FROM role', function(err, res) {
+                if (err) throw (err);
+                let filteredRole = res.filter(function(res) {
+                    return res.title == role;
+                });
+            
+            
+                        
+                let query = "INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?)";
+                let values = [answer.firstName, answer.lastName, answer.role_id]
+                console.log(values);
+                
+                connection.query(query, values,
+                    function(err, res, fields) {
+                        console.log(`You have added this employee: ${(values[0]).toUpperCase()}.`)
+                    })
+                viewEmployees();
+                       
+            })
+        })
+            // connection.end();
+    })
+       
+   
+}
+
+function updateRole() {
+    connection.query('SELECT * FROM employee', function(err, result) {
+        if (err) throw (err);
+    inquirer
+        .prompt([
+          {
+            name: "employeeName",
+            type: "list",
+// is there a way to make the options here the results of a query that selects all departments?`
+            message: "Which employee's role is changing?",
+            choices: function() {
+             employeeArray = [];
+                result.forEach(result => {
+                    employeeArray.push(
+                        result.last_name
+                    );
+                })
+                return employeeArray;
+              }
+          }
+          ]) 
+// in order to get the id here, i need a way to grab it from the departments table 
+        .then(function(answer) {
+        console.log(answer);
+        const name = answer.employeeName;
+    
+        connection.query("SELECT * FROM role", function(err, res) {
+                inquirer
+                .prompt ([
+                    {
+                        name: "role",
+                        type: "list",
+                        message: "What is their new role?",
+                        choices: function() {
+                            rolesArray = [];
+                            res.forEach(res => {
+                                rolesArray.push(res.title)
+                            })
+                            return rolesArray;
+                        }
+                    }
+                ]).then(function(rolesAnswer) {
+                    const role = rolesAnswer.role;
+                    console.log(rolesAnswer.role);
+                connection.query('SELECT * FROM role WHERE title = ?', [role], function(err, res) {
+                if (err) throw (err);
+                    let roleId = res[0].id;
+                    let query = "UPDATE employee SET role_id ? WHERE last_name ?";
+                    let values = [roleId, name]
+                    console.log(values);
+                     connection.query(query, values,
+                         function(err, res, fields) {
+                         console.log(`You have updated ${name}'s role to ${role}.`)
+                        });
+                        viewEmployees();
+                        });
+                     });
+                });
+       });
+       connection.end();
 });
 
-function runSearch() {
-  inquirer
-    .prompt({
-      name: "action",
-      type: "rawlist",
-      message: "What would you like to do?",
-      choices: [
-        "Find songs by artist",
-        "Find all artists who appear more than once",
-        "Find data within a specific range",
-        "Search for a specific song",
-        "Find artists with a top song and top album in the same year"
-      ]
-    })
-    .then(function(answer) {
-      switch (answer.action) {
-      case "Find songs by artist":
-        artistSearch();
-        break;
-
-      case "Find all artists who appear more than once":
-        multiSearch();
-        break;
-
-      case "Find data within a specific range":
-        rangeSearch();
-        break;
-
-      case "Search for a specific song":
-        songSearch();
-        break;
-
-      case "Find artists with a top song and top album in the same year":
-        songAndAlbumSearch();
-        break;
-      }
-    });
-}
-
-function artistSearch() {
-  inquirer
-    .prompt({
-      name: "artist",
-      type: "input",
-      message: "What artist would you like to search for?"
-    })
-    .then(function(answer) {
-      var query = "SELECT position, song, year FROM top5000 WHERE ?";
-      connection.query(query, { artist: answer.artist }, function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-        }
-        runSearch();
-      });
-    });
-}
-
-function multiSearch() {
-  var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-  connection.query(query, function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i].artist);
-    }
-    runSearch();
-  });
-}
-
-function rangeSearch() {
-  inquirer
-    .prompt([
-      {
-        name: "start",
-        type: "input",
-        message: "Enter starting position: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      },
-      {
-        name: "end",
-        type: "input",
-        message: "Enter ending position: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      }
-    ])
-    .then(function(answer) {
-      var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-      connection.query(query, [answer.start, answer.end], function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log(
-            "Position: " +
-              res[i].position +
-              " || Song: " +
-              res[i].song +
-              " || Artist: " +
-              res[i].artist +
-              " || Year: " +
-              res[i].year
-          );
-        }
-        runSearch();
-      });
-    });
-}
-
-function songSearch() {
-  inquirer
-    .prompt({
-      name: "song",
-      type: "input",
-      message: "What song would you like to look for?"
-    })
-    .then(function(answer) {
-      console.log(answer.song);
-      connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
-        console.log(
-          "Position: " +
-            res[0].position +
-            " || Song: " +
-            res[0].song +
-            " || Artist: " +
-            res[0].artist +
-            " || Year: " +
-            res[0].year
-        );
-        runSearch();
-      });
-    });
-}
-
-function songAndAlbumSearch() {
-  inquirer
-    .prompt({
-      name: "artist",
-      type: "input",
-      message: "What artist would you like to search for?"
-    })
-    .then(function(answer) {
-      var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-      query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
-      query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
-
-      connection.query(query, [answer.artist, answer.artist], function(err, res) {
-        console.log(res.length + " matches found!");
-        for (var i = 0; i < res.length; i++) {
-          console.log(
-            i+1 + ".) " +
-              "Year: " +
-              res[i].year +
-              " Album Position: " +
-              res[i].position +
-              " || Artist: " +
-              res[i].artist +
-              " || Song: " +
-              res[i].song +
-              " || Album: " +
-              res[i].album
-          );
-        }
-
-        runSearch();
-      });
-    });
-}
+};
